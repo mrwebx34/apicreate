464f6c9e59936ab5084c3e22c2007650c2687753
ranjan rath <ranjankumarrath344@gmail.com>
2024-08-15 00:12:42 +0530

    apiupdate

--- api-insert.php
+++ api-insert.php
@@ -1,14 +1,18 @@
 <?php
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin:*');
+header('Access-Control-Allow-Method:POST');
+header('Access-Control-Allow-Headers:Access-Control-Allow-Headers,Access-Control-Allow-Method, Content-Type,Authorization,X-Requested-With');
 include "db-conn.php";
 
 $data =json_decode(file_get_contents("php://input"),true);
-$student_id=$data['sid'];
+$student_name=$data['sname'];
+$student_class=$data['sclass'];
+$student_age=$data['sage'];
 if (isset($data['sid']) && !empty($data['sid'])) {
     $student_id = $data['sid'];
 
-$sql ="SELECT * FROM students WHERE id={$student_id}";
+$sql ="INSERT INTO students( name , class , age) VALUES('{$student_name}','{$student_class}','{$student_age}')";
 
 $result=mysqli_query($conn,$sql) or die('Sql query failed');
 
