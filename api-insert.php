d21883b4b54723b2efe82140fe8afc61e0d7c96b
ranjan rath <ranjankumarrath344@gmail.com>
2024-08-17 11:11:48 +0530

    apiinsert

--- api-insert.php
+++ api-insert.php
@@ -9,23 +9,19 @@ $data =json_decode(file_get_contents("php://input"),true);
 $student_name=$data['sname'];
 $student_class=$data['sclass'];
 $student_age=$data['sage'];
-if (isset($data['sid']) && !empty($data['sid'])) {
+
     $student_id = $data['sid'];
 
 $sql ="INSERT INTO students( name , class , age) VALUES('{$student_name}','{$student_class}','{$student_age}')";
 
 $result=mysqli_query($conn,$sql) or die('Sql query failed');
 
-if(mysqli_num_rows($result)>0){
-    $output=mysqli_fetch_all($result,MYSQLI_ASSOC);
-    echo json_encode($output);
-
+if(mysqli_query($conn,$sql)){
+  
+   echo(json_encode(array('message'=>'Students record inserted.','status'=>true)));
 }
 else{
-     echo(json_encode(array('message'=>'no records found.','status'=>false)));
-}}
-else {
-    
-    echo json_encode(array('message' => 'Invalid student ID.', 'status' => false));
+     echo(json_encode(array('message'=>'Students record not inserted.','status'=>false)));
 }
+
 ?>
